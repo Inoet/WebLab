0c012e77cdddb47a4208e3c347853068d9ae8ace
Inoet <lenin110@inbox.ru>
2024-12-26 20:06:06 +0300

    ы

--- obj/Debug/net8.0/gestion.AssemblyInfo.cs
+++ obj/Debug/net8.0/gestion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gestion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d97db7ce3527d2ad552f306af797258a2a468f3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("gestion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gestion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
